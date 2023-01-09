9b729ca152d3530bb0109c65253f5f1cb6e7a551
Daniele Conti <fourlastor@users.noreply.github.com>
2023-01-09 19:55:35 +0000

    Adjust html buildscript

--- html/build.gradle.kts
+++ html/build.gradle.kts
@@ -1,12 +1,25 @@
+import org.akhikhl.gretty.AppBeforeIntegrationTestTask
+import org.wisepersist.gradle.plugins.gwt.GwtSuperDev
+
+@Suppress(
+    // known false positive: https://youtrack.jetbrains.com/issue/KTIJ-19369
+    "DSL_SCOPE_VIOLATION"
+)
 plugins {
     java
-    id( "org.wisepersist.gwt") version "1.1.16"
-    id ("war")
-    id ("org.gretty") version "4.0.3"
+    war
+    alias(libs.plugins.gretty)
+    alias(libs.plugins.gwt)
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
+
+war.webAppDirName = "webapp"
+
+tasks.named("clean", Delete::class.java) {
+    delete(project.file("war"))
+}
 
 gwt {
     gwtVersion="2.8.2" // Should match the gwt version used for building the gwt backend
@@ -17,87 +30,77 @@ gwt {
     src += files(File(project(":core").projectDir, "build/generated/sources/annotationProcessor/java/main"))
     modules("io.github.fourlastor.game.client.GdxDefinition")
     devModules("io.github.fourlastor.game.client.GdxDefinitionSuperdev")
-//    project.webAppDirName = 'webapp'
-//
     compiler.strict = true
     compiler.disableCastChecking = true
+}
+
+gretty {
+    httpPort = 8080
+    resourceBase = "$buildDir/gwt/draftOut"
+    contextPath = "/"
+    portPropertiesFileName = "TEMP_PORTS.properties"
+}
+
+tasks.create("startHttpServer", Copy::class.java) {
+    dependsOn(tasks.named("draftCompileGwt"))
+    from("webapp", "war")
+    into(gretty.resourceBase)
+}
+
+tasks.create("beforeRun", AppBeforeIntegrationTestTask::class.java) {
+    // The next line allows ports to be reused instead of
+    // needing a process to be manually terminated.
+    file("build/TEMP_PORTS.properties").delete()
+    dependsOn(tasks.named("startHttpServer"))
+    interactive = false
+    integrationTestTask("superDev")
 
 }
 
-//import org.wisepersist.gradle.plugins.gwt.GwtSuperDev
-//import org.akhikhl.gretty.AppBeforeIntegrationTestTask
+tasks.create("superDev", GwtSuperDev::class.java) {
+    dependsOn(tasks.named("startHttpServer"))
+    doFirst {
+        gwt.modules = gwt.devModules
+    }
+}
 
-//gretty.httpPort = 8080
-//gretty.resourceBase = project.buildDir.path + "/gwt/draftOut"
-//gretty.contextPath = "/"
-//gretty.portPropertiesFileName = "TEMP_PORTS.properties"
-//
-//task startHttpServer () {
-//    dependsOn draftCompileGwt
-//
-//    doFirst {
-//        copy {
-//            from "webapp"
-//            into gretty.resourceBase
-//        }
-//
-//        copy {
-//            from "war"
-//            into gretty.resourceBase
-//        }
-//    }
-//}
-//
-//task beforeRun(type: AppBeforeIntegrationTestTask, dependsOn: startHttpServer) {
-//    // The next line allows ports to be reused instead of
-//    // needing a process to be manually terminated.
-//    file("build/TEMP_PORTS.properties").delete()
-//    // Somewhat of a hack; uses Gretty's support for wrapping a task in
-//    // a start and then stop of a Jetty server that serves files while
-//    // also running the SuperDev code server.
-//    integrationTestTask 'superDev'
-//
-//    interactive false
-//}
-//
-//task superDev (type: GwtSuperDev) {
-//    dependsOn startHttpServer
-//    doFirst {
-//        gwt.modules = gwt.devModules
-//    }
-//}
-//
-//task dist(dependsOn: [clean, compileGwt]) {
-//    doLast {
-//        file("build/dist").mkdirs()
-//        copy {
-//            from "build/gwt/out"
-//            into "build/dist"
-//        }
-//        copy {
-//            from "webapp"
-//            into "build/dist"
-//            }
-//        copy {
-//            from "war"
-//            into "build/dist"
-//        }
-//    }
-//}
-//
-//task addSource {
-//    doLast {
-//        sourceSets.main.compileClasspath += files(project(':core').sourceSets.main.allJava.srcDirs)
-//    }
-//}
-//
-//tasks.compileGwt.dependsOn(addSource)
-//tasks.draftCompileGwt.dependsOn(addSource)
-//tasks.checkGwt.dependsOn(addSource)
-//checkGwt.war = file("war")
-//
-//sourceCompatibility = 1.8
-////sourceSets.srcDirs = [ "src/" ]
+val distDir = "$buildDir/dist"
+
+tasks.create("dist") {
+    dependsOn(tasks.named("clean"), tasks.named("compileGwt"))
+    doLast {
+        copy {
+            from("$buildDir/gwt/out") {
+                exclude( "**/*.symbolMap") // Not used by a dist, and these can be large.
+            }
+            into(distDir)
+        }
+        copy {
+            from(project.file("webapp")) {
+                exclude("index.html", "refresh.png")
+            }
+            into(distDir)
+        }
+        copy {
+            from(project.file("webapp")) {
+                include("index.html")
+                filter { it.replace(Regex("<a class=\"superdev\" .+"), "") }
+                // This does not modify the original index.html, only the copy in the dist.
+                // If you decide to manually remove or comment out the superdev button from index.html, you should also
+                // either remove or comment out only the "filter" line above this.
+            }
+            into(distDir)
+        }
+        copy {
+            from(project.file("war"))
+            into(distDir)
+        }
+    }
+}
+
+java.sourceSets.main.configure {
+    compileClasspath += files(project(":core").sourceSets.main.get().allJava.srcDirs)
+}
 
 dependencies {
     implementation (project(":core"))
@@ -114,6 +117,7 @@ dependencies {
     sources(libs.gdx.controllers.gwt)
     sources(libs.ashley)
     sources(libs.textratypist)
+    sources(libs.regexodus)
 }
 
 fun DependencyHandlerScope.sources(
