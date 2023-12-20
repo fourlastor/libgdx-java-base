8f435a0c73f01105e6116dcb5ce9265fcaa26ba7
Daniele Conti <fourlastor@users.noreply.github.com>
2023-12-20 20:16:32 +0100

    Remove stuff from GdxGame

--- core/src/main/java/io/github/fourlastor/game/GdxGame.java
+++ core/src/main/java/io/github/fourlastor/game/GdxGame.java
@@ -15,54 +15,19 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
-public class GdxGame extends Game implements Router {
+public class GdxGame extends Game {
 
-    private final InputMultiplexer multiplexer;
-
-    private final LevelComponent.Builder levelScreenFactory;
-    private final IntroComponent.Builder introScreenFactory;
-    private Screen pendingScreen = null;
 
     @Inject
-    public GdxGame(
-            InputMultiplexer multiplexer,
-            LevelComponent.Builder levelScreenFactory,
-            IntroComponent.Builder introScreenFactory) {
-        this.multiplexer = multiplexer;
-        this.levelScreenFactory = levelScreenFactory;
-        this.introScreenFactory = introScreenFactory;
-        Harlequin.LIST_CREATOR = new Harlequin.ListCreator() {
-            @Override
-            public <T> List<T> newList() {
-                return new ObjectList<>();
-            }
-
-            @Override
-            public <T> List<T> newList(int size) {
-                return new ObjectList<>(size);
-            }
-        };
+    public GdxGame() {
     }
 
     @Override
     public void create() {
-        //        if (Gdx.app.getType() != Application.ApplicationType.Android) {
-        //
-        //            Cursor customCursor =
-        //                    Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("images/included/whitePixel.png")),
-        // 0, 0);
-        //            Gdx.graphics.setCursor(customCursor);
-        //        }
-        Gdx.input.setInputProcessor(multiplexer);
-        goToIntro();
     }
 
     @Override
     public void render() {
-        if (pendingScreen != null) {
-            setScreen(pendingScreen);
-            pendingScreen = null;
-        }
         super.render();
     }
 
@@ -70,13 +35,4 @@ public class GdxGame extends Game implements Router {
         return GameComponent.component().game();
     }
 
-    @Override
-    public void goToIntro() {
-        pendingScreen = introScreenFactory.router(this).build().screen();
-    }
-
-    @Override
-    public void goToLevel() {
-        pendingScreen = levelScreenFactory.router(this).build().screen();
-    }
 }
